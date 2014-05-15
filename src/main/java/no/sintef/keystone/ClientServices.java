a19f9d2020b74c028baba077a2baf0dd245d3743
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2014-05-15 16:22:29 +0200

    Initial commit

---
+++ src/main/java/no/sintef/keystone/ClientServices.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package no.sintef.keystone;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
+import javax.json.JsonWriter;
+
+/**
+ *
+ * @author kjetilo
+ */
+public class ClientServices {
+    final private URL url;
+    
+    public ClientServices(URL url) {
+        this.url = url;
+    }
+    
+    /**
+     * Logs the user in and returns a sessiontoken.
+     * @param username the username
+     * @param password the password to use.
+     * @note Defaults to the 
+     * @return 
+     */
+    String login(String username, String password, String tenant) throws IOException {
+        
+        
+        HttpURLConnection connection = (HttpURLConnection)(new URL(url, "v2.0/tokens")).openConnection();
+        JsonObject inputObject = Json.createObjectBuilder()
+                .add("auth", Json.createObjectBuilder()
+                    .add("passwordCredentials", Json.createObjectBuilder()
+                        .add("username", username)
+                        .add("password", password))
+                .add("tenantName", tenant)).build();
+        
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Accept", "application/json");
+        
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+        
+        connection.setRequestMethod("POST");
+        
+        OutputStream output = connection.getOutputStream();
+        
+        
+        JsonWriter writer = Json.createWriter(output);
+        writer.write(inputObject);
+        writer.close();
+        output.close();
+        
+        InputStream input = connection.getInputStream();
+        
+        JsonReader reader = Json.createReader(input);
+        
+        JsonObject outputObject = reader.readObject();
+        reader.close();
+        input.close();
+        
+        return outputObject.getJsonObject("access").getJsonObject("token").getString("id");
+    }
+    
+    /**
+     * Checks if the given user is logged in with the given token.
+     * @param userToken the token
+     * @return the userName of the current user
+     */
+    public String getUsername(String userToken, String tenant) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection)(new URL(url, "v2.0/tokens").openConnection());
+        
+       connection.setRequestProperty("Accept", "application/json");
+        
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+        
+        connection.setRequestMethod("POST");
+        JsonObject inputObject = Json.createObjectBuilder()
+                .add("auth", Json.createObjectBuilder()
+                    .add("token", Json.createObjectBuilder()
+                        .add("id", userToken))
+                .add("tenantName", tenant)).build();
+        
+        connection.setRequestProperty("Content-Type", "application/json");
+        
+        OutputStream output = connection.getOutputStream();
+        JsonWriter writer = Json.createWriter(output);
+        writer.write(inputObject);
+        writer.close();
+        output.close();
+        
+        int response = connection.getResponseCode();
+        
+        if(response == 501) {
+            throw new RuntimeException("Bad authorization");
+        }
+        
+        if(response != 203 && response != 200) {
+            throw new RuntimeException("Could not get username");
+        }
+        InputStream input = connection.getInputStream();
+        JsonReader reader = Json.createReader(input);
+        
+        JsonObject outputObject = reader.readObject();
+        reader.close();
+        input.close();
+        
+        return outputObject.getJsonObject("access").getJsonObject("user").getString("username");
+    }
+}
